75da401c9fbf2a65a9ef37b24f5e1e974dbce0c1
Anara Orazalina <orazalina.anara@inbox.ru>
2021-04-05 23:07:02 +0600

    junit4-platform added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,13 +1,43 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @org.junit.Test
+    @org.junit.jupiter.api.Test
     public void remain1001() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void remain1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remain999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void remain10010() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
         int actual = service.remain(amount);
@@ -17,17 +47,17 @@ public class CashbackHackServiceTest {
     }
 
     @org.junit.Test
-    public void remain1000() {
+    public void remain10000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(actual,expected);
     }
 
     @org.junit.Test
-    public void remain999() {
+    public void remain9990() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
         int actual = service.remain(amount);
